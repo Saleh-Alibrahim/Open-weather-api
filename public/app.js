d8f08b36d4d973ac7a746cf3de11909116444240
Saleh Alibrahim <s.albrahim@outlook.com>
2020-07-31 13:50:36 +0300

    feat: done from calling the open weather api

--- public/app.js
+++ public/app.js
@@ -1,5 +1,47 @@
 /* Global Variables */
+const localhost = 'http://localhost:5000';
+const openWeatherUrl = 'http://api.openweathermap.org/data/2.5/weather';
+const openWeatherApiKey = 'ca637ed5ccc3fe37d3c21ecfef1a297c';
+const feelings = document.getElementById('feelings');
+const zip = document.getElementById('zip');
+
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+
+
+
+document.getElementById('generate').addEventListener('click', async function (e) {
+
+    // Prevent the default of submit action
+    e.preventDefault();
+
+    // Get The feelings and zip code
+    feelingsValue = feelings.textContent;
+    zipValue = zip.textContent;
+
+    // If either is empty show error
+    if (!feelingsValue || !zipValue) {
+        showErrorMessage();
+        return;
+    }
+    else {
+        hideErrorMessage();
+    }
+    // Call the open weather api
+    const response = await fetch(`${openWeatherUrl}?zip=${zipValue}&appid=${openWeatherApiKey}&units=imperial`);
+
+    // Convert the result to json
+    const jsonRes = response.json();
+
+
+});
+
+function showErrorMessage() {
+
+}
+function hideErrorMessage() {
+
+}
--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,11 +1,25 @@
 const express = require('express');
-
+const ErrorResponse = require('../utils/errorResponse');
 
 const router = express.Router();
 
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
 
-router.get('/', (req, res, next) => {
+// Add new Data
+router.post('/', (req, res, next) => {
 
+});
+
+// Get All Data
+router.get('/all', (req, res, next) => {
+
+});
+
+// Catch the 404 routes
+router.get('*', (req, res, next) => {
+    // Return error message
+    next(new ErrorResponse('Incorrect route use either / or /all '), 404);
 });
 
 module.exports = router;
