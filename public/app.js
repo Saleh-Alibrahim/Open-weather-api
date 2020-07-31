951bb883cde525a2e188730e8cce00a7203366e5
Saleh Alibrahim <s.albrahim@outlook.com>
2020-07-31 16:11:27 +0300

    feat: Add the frontend js

--- public/app.js
+++ public/app.js
@@ -4,6 +4,8 @@ const openWeatherUrl = 'http://api.openweathermap.org/data/2.5/weather';
 const openWeatherApiKey = 'ca637ed5ccc3fe37d3c21ecfef1a297c';
 const feelings = document.getElementById('feelings');
 const zip = document.getElementById('zip');
+const errorMessage = document.getElementById('error-message');
+const entryHolder = document.getElementById('entryHolder');
 
 
 // Create a new date instance dynamically with JS
@@ -15,33 +17,82 @@ let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
 
 document.getElementById('generate').addEventListener('click', async function (e) {
 
-    // Prevent the default of submit action
-    e.preventDefault();
+    try {
 
-    // Get The feelings and zip code
-    feelingsValue = feelings.textContent;
-    zipValue = zip.textContent;
 
-    // If either is empty show error
-    if (!feelingsValue || !zipValue) {
-        showErrorMessage();
-        return;
+        // Prevent the default of submit action
+        e.preventDefault();
+
+        // Get The feelings and zip code
+        feelingsValue = feelings.value;
+        zipValue = zip.value;
+
+        // If either is empty show error
+        if (!feelingsValue || !zipValue) {
+            showErrorMessage('Please fill all the fields');
+            return;
+        }
+        else {
+            hideErrorMessage();
+        }
+
+        // Call the open weather api
+        let openWeatherResponse = await fetch(`${openWeatherUrl}?zip=${zipValue}&appid=${openWeatherApiKey}&units=imperial`);
+
+        // Convert the result to json
+        let jsonOpenWeatherResponse = await openWeatherResponse.json();
+
+        // Show error message if wrong zip number 
+        if (jsonOpenWeatherResponse.cod != 200) {
+            showErrorMessage(jsonOpenWeatherResponse.message);
+            return;
+        }
+        else {
+            hideErrorMessage();
+        }
+
+        // Get the temperature from the response
+        let temperature = jsonOpenWeatherResponse.main.temp;
+
+
+        // Store all the fields to one object
+        let projectData = {
+            date: newDate,
+            feelings: feelingsValue,
+            zip: zipValue,
+            temperature: temperature
+        };
+
+        // Call the server
+        let ServerResponse = await fetch('/', {
+            method: 'POST',
+            body: JSON.stringify(projectData)
+        });
+
+        let JsonServerResponse = await ServerResponse.json();
+
+        if (JsonServerResponse.success) {
+            entryHolder.children[0].innerHTML = `date = ${projectData.date}`;
+            entryHolder.children[1].innerHTML = `temp = ${projectData.temperature}`;
+            entryHolder.children[2].innerHTML = `content = ${projectData.feelings}`;
+        }
+
     }
-    else {
-        hideErrorMessage();
+    catch (error) {
+        console.log(error);
     }
-    // Call the open weather api
-    const response = await fetch(`${openWeatherUrl}?zip=${zipValue}&appid=${openWeatherApiKey}&units=imperial`);
-
-    // Convert the result to json
-    const jsonRes = response.json();
 
 
 });
 
-function showErrorMessage() {
-
+function showErrorMessage(msg) {
+    errorMessage.innerHTML = msg;
+    if (!errorMessage.classList.contains('show-message')) {
+        errorMessage.classList.add('show-message');
+    }
 }
 function hideErrorMessage() {
-
+    if (errorMessage.classList.contains('show-message')) {
+        errorMessage.classList.remove('show-message');
+    }
 }
--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -4,22 +4,38 @@ const ErrorResponse = require('../utils/errorResponse');
 const router = express.Router();
 
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+let projectData = [];
+
+
+
 
 // Add new Data
 router.post('/', (req, res, next) => {
+
+    // Store the data to the array
+    projectData.push(req.body);
+
+    // Return success
+    res.json({ success: true });
 
 });
 
 // Get All Data
 router.get('/all', (req, res, next) => {
 
+    // Return all the data
+    res.json(projectData);
 });
 
 // Catch the 404 routes
 router.get('*', (req, res, next) => {
+
+    // do not show the error if rendering the main page
+    if (req.url == '/kj' && req.method == 'GET') {
+        return;
+    }
     // Return error message
-    next(new ErrorResponse('Incorrect route use either / or /all '), 404);
+    next(new ErrorResponse('Incorrect route use either / or /all ', 404));
 });
 
 module.exports = router;
